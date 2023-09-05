bad21678863a434f1f633242f4bb4c99853c27ec
Gilford0306 <Gilford0306@gmail.com>
2023-09-05 17:54:20 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PixrlBayParsing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(40, 718);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(597, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(686, 715);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(26, 28);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(805, 621);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(1217, 712);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 35);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoScroll = true;
+            this.flowLayoutPanel2.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(837, 28);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(805, 650);
+            this.flowLayoutPanel2.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1655, 779);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "pi";
+            this.flowLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using System.Windows.Forms;
+
+namespace PixrlBayParsing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            saveFileDialog1.Filter = "Picture files(*.jpg)|*.jpg|All files(*.*)|*.*";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text.ToLower().Replace(' ', '+');
+            string url = $"https://pixabay.com/api/?key=39207646-8ccbcc7ff50179f772c7bccff&q={str}";
+            WebClient client = new WebClient();
+            string result = client.DownloadString(url);
+            Root root = JsonSerializer.Deserialize<Root>(result);
+            
+            for (int i = 0; i < 20; i++)
+            {
+                try { client.DownloadFile(root.hits[i].largeImageURL, $"{i}.jpg");  }     
+
+                catch { }
+                
+            }
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    PictureBox picture = new PictureBox
+            //    {
+            //        Name = $"pictureBox{i}",
+            //        ImageLocation = $@"{i}.jpg",
+            //        SizeMode = PictureBoxSizeMode.StretchImage
+            //    };
+            //    pictureBox.Controls.Add(picture);
+            //}
+            //PictureBox picture = new PictureBox
+            //{
+            //    Name = "pictureBox",
+            //    ImageLocation = @"0.jpg",
+            //    SizeMode = PictureBoxSizeMode.StretchImage
+            //};
+
+            //pictureBox1.Controls.Add(picture);
+            //picture = new PictureBox
+            //{
+            //    Name = "pictureBox",
+            //    ImageLocation = @"1.jpg",
+            //    SizeMode = PictureBoxSizeMode.StretchImage
+            //};
+            //pictureBox2.Controls.Add(picture);
+
+
+            List<PictureBox> picturebox = new List<PictureBox>();
+            var CurDir = Directory.GetCurrentDirectory();
+            DirectoryInfo directoryInfo = new DirectoryInfo(CurDir);
+            var recentpics = directoryInfo.GetFiles().ToList();
+            var y = 10;
+            flowLayoutPanel1.Controls.Clear();
+            for (int i = 0; i < 20; i++)
+                {
+                try
+                {
+                    var pb = new PictureBox();
+
+                    pb.Location = new Point(picturebox.Count * 120 + 20, y);
+                    pb.Size = new Size(200, 120);
+                    pb.Name = i.ToString();
+                    try
+                    {
+
+                        pb.Image = Image.FromFile($"{i}.jpg");
+                    }
+                    catch (OutOfMemoryException) { continue; }
+                    pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                    flowLayoutPanel1.Controls.Add(pb);
+                    picturebox.Add(pb);
+                    pb.Click += Pb_Click;
+                }
+                catch (Exception)
+                {
+
+                    
+                }
+
+            }
+        }
+
+        private void Pb_Click(object sender, EventArgs e)
+        {
+
+            flowLayoutPanel2.Controls.Clear();
+            pictureBox1.Image = Image.FromFile($"{(sender as PictureBox).Name.ToString()}.jpg");
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            flowLayoutPanel2.Controls.Add(pictureBox1);
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog() { Filter = @"JPG|*.jpg" })
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        pictureBox1.Image.Save(saveFileDialog.FileName);
+                        MessageBox.Show("File is save");
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("File not saved");
+
+                    }
+                    
+                }
+            }
+        }
+    }
+}
